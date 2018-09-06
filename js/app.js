6c9932ca25a79fabc923b9a1de83971fbe5daf28
unknown <vmirella_17@hotmail.com>
2018-09-06 00:31:49 -0500

    Cambio de color al menu

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $('.menu-link').click((event) => {
+    /*event.preventDefault();
+
+    const link = $(this).attr('href');
+    $('body,html').stop(true,true).animate({        
+      scrollTop: $(link).offset().top
+    },1000);*/
+  });
+});
