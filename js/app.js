61ec4e55b1a22084583ea9f3aa8a49167ef52ce3
unknown <vmirella_17@hotmail.com>
2018-09-09 19:29:39 -0500

    Implementacion de la maqueta de la seccion de habilidades

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(document).ready(() => {
-  $('.menu-link').click((event) => {
-    /*event.preventDefault();
 
-    const link = $(this).attr('href');
+  $('.menu-link').click((e) => {
+    e.preventDefault();
+
+    const link = $(e.currentTarget).attr('href');
     $('body,html').stop(true,true).animate({        
       scrollTop: $(link).offset().top
-    },1000);*/
+    },1000);
   });
+
 });
